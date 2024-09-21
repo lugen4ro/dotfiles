a55447b4ac089294735cb0a789d69950f3419443
lugen4ro <54658759+lugen4ro@users.noreply.github.com>
2024-09-21 23:17:31 +0900

    feat: nvim - competitive programming related

--- nvim/lua/autocommand.lua
+++ nvim/lua/autocommand.lua
@@ -8,8 +8,8 @@
 -- just using vim.opt.formatoptions:remove({'r', 'c', 'o'}) didn't work because it was overwritten
 -- you can see where the variable was last changed with `:verbose set formatoptions?`
 vim.api.nvim_create_autocmd("BufEnter", {
-	pattern = "",
-	command = "set fo-=r fo-=c fo-=o",
+    pattern = "",
+    command = "set fo-=r fo-=c fo-=o",
 })
 
 -----------------------------------------------------------------------------
@@ -17,10 +17,10 @@ vim.api.nvim_create_autocmd("BufEnter", {
 -----------------------------------------------------------------------------
 vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
-	group = "YankHighlight",
-	callback = function()
-		vim.highlight.on_yank({ higroup = "IncSearch", timeout = "400" })
-	end,
+    group = "YankHighlight",
+    callback = function()
+        vim.highlight.on_yank({ higroup = "IncSearch", timeout = "400" })
+    end,
 })
 
 -----------------------------------------------------------------------------
@@ -29,8 +29,8 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 
 -- Auto save input file for AtCoder
 vim.api.nvim_create_autocmd("BufLeave", {
-	pattern = "/home/gen4ro/code/dsa/cpp/atcoder/input",
-	command = "w",
+    pattern = "/home/gen4ro/code/dsa/cpp/atcoder/input",
+    command = "w",
 })
 
 -----------------------------------------------------------------------------
@@ -40,8 +40,8 @@ vim.api.nvim_create_autocmd("BufLeave", {
 -- Have set a custom keymapping for <CR> in normal mode which interferes with quickfix keys
 -- So in quickfix window, disable the <CR> keymapping (<CR> is used to open location)
 vim.api.nvim_create_autocmd("BufReadPost", {
-	pattern = "quickfix",
-	command = "nnoremap <buffer> <CR> <CR>",
+    pattern = "quickfix",
+    command = "nnoremap <buffer> <CR> <CR>",
 })
 
 -----------------------------------------------------------------------------
@@ -50,22 +50,22 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 
 -- Overwrite color scheme
 vim.api.nvim_create_autocmd("ColorScheme", {
-	pattern = "*",
-	desc = "prevent colorscheme clears self-defined DAP icon colors.",
-	callback = function()
-		---------- Telescope ----------
-		-- Color for matches that are found while typing in Telescope
-		vim.api.nvim_set_hl(0, "TelescopeMatching", { ctermbg = 0, fg = "#fab387", bg = "" })
+    pattern = "*",
+    desc = "prevent colorscheme clears self-defined DAP icon colors.",
+    callback = function()
+        ---------- Telescope ----------
+        -- Color for matches that are found while typing in Telescope
+        vim.api.nvim_set_hl(0, "TelescopeMatching", { ctermbg = 0, fg = "#fab387", bg = "" })
 
-		-- Color for filenames in the results of telescope
-		vim.api.nvim_set_hl(0, "TelescopeFileMatch", { ctermbg = 0, fg = "#89dceb", bg = "" })
+        -- Color for filenames in the results of telescope
+        vim.api.nvim_set_hl(0, "TelescopeFileMatch", { ctermbg = 0, fg = "#89dceb", bg = "" })
 
-		---------- DAP ----------
-		vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#f55151", bg = "#31353f" })
-		vim.api.nvim_set_hl(0, "DapBreakpointCondition", { ctermbg = 0, fg = "#f79b4a", bg = "#31353f" })
-		vim.api.nvim_set_hl(0, "DapLogPoint", { ctermbg = 0, fg = "#61afef", bg = "#31353f" })
-		vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379", bg = "#31353f" })
-	end,
+        ---------- DAP ----------
+        vim.api.nvim_set_hl(0, "DapBreakpoint", { ctermbg = 0, fg = "#f55151", bg = "#31353f" })
+        vim.api.nvim_set_hl(0, "DapBreakpointCondition", { ctermbg = 0, fg = "#f79b4a", bg = "#31353f" })
+        vim.api.nvim_set_hl(0, "DapLogPoint", { ctermbg = 0, fg = "#61afef", bg = "#31353f" })
+        vim.api.nvim_set_hl(0, "DapStopped", { ctermbg = 0, fg = "#98c379", bg = "#31353f" })
+    end,
 })
 
 -----------------------------------------------------------------------------
@@ -74,19 +74,19 @@ vim.api.nvim_create_autocmd("ColorScheme", {
 
 -- Do not make a backup before overwriting a file, so that parcel can recognize file changes
 vim.api.nvim_create_autocmd("BufEnter", {
-	pattern = { "*.js", "*.css", "*.html" },
-	desc = "Do not make a backup before overwriting a file, so that parcel can recognize file changes",
-	callback = function()
-		vim.opt_local.writebackup = false
-	end,
+    pattern = { "*.js", "*.css", "*.html" },
+    desc = "Do not make a backup before overwriting a file, so that parcel can recognize file changes",
+    callback = function()
+        vim.opt_local.writebackup = false
+    end,
 })
 
 -- In help files, jump to tag definition with gd insted of default ctrl-]
 vim.api.nvim_create_autocmd({ "FileType" }, {
-	pattern = { "help" },
-	callback = function(opts)
-		vim.keymap.set("n", "gd", "<C-]>", { silent = true, buffer = opts.buf })
-	end,
+    pattern = { "help" },
+    callback = function(opts)
+        vim.keymap.set("n", "gd", "<C-]>", { silent = true, buffer = opts.buf })
+    end,
 })
 
 -- TODO: Keep original cursor position after block yank
@@ -101,14 +101,14 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 --------- Auto change IME when leaving insert mode (have to set environamental variable zenhan in .zshrc to point to zenhan.exe)
 -- Japanese
 vim.api.nvim_create_autocmd("InsertLeave", {
-	pattern = "*",
-	desc = "",
-	command = "call system('${zenhan} 0')",
+    pattern = "*",
+    desc = "",
+    command = "call system('${zenhan} 0')",
 })
 vim.api.nvim_create_autocmd("CmdlineLeave", {
-	pattern = "*",
-	desc = "",
-	command = "call system('${zenhan} 0')",
+    pattern = "*",
+    desc = "",
+    command = "call system('${zenhan} 0')",
 })
 
 -- -- [Conform] format on save, except for C++
